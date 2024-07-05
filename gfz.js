1a28f05bf4803fe177d542e1561b97f4b7e25eaf
GuyCui <18024642@qq.com>
2024-07-05 01:58:46 +0000

    功夫者试看

---
+++ gfz.js
@@ -0,0 +1,29 @@
+/*
+
+功夫者试看
+
+[rewrite_local]
+
+^https:\/\/mp\.gongfuzhe\.com\/api\/(course\/courseDetail) url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/ghsx.js
+
+[mitm]
+hostname = mp.gongfuzhe.com
+*/
+
+let body = $response.body;
+let url = $request.url;
+
+if (url.indexOf("http://mp.gongfuzhe.com/api/course/courseDetail") !== -1) {
+    let data = JSON.parse(body);
+
+    // 修改lesson数组中的free字段
+    if (data.data && data.data.lesson) {
+        data.data.lesson.forEach(lesson => {
+            lesson.free = 1;
+        });
+    }
+
+    body = JSON.stringify(data);
+}
+
+$done({ body: body });
